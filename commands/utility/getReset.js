44f1aec522f49cd1f9eea0b22a359f22d20b3654
agu <iba.agustin@gmail.com>
2023-12-26 12:29:31 -0300

    fix reset day update

--- commands/utility/getReset.js
+++ commands/utility/getReset.js
@@ -1,20 +1,20 @@
 const { SlashCommandBuilder } = require("discord.js");
 const getTimes = require("../../utils/time");
 
+const rightNow = Date.now();
+let nextReset = Date.parse("21 Dec 2023 10:00:00 EST");
+
+// Once rightNow is equal or lower than nextReset, nextReset gains 72 hours
+while (rightNow >= nextReset) {
+  nextReset = nextReset + 72 * 3600000;
+}
+
 module.exports = {
   cooldown: 5,
   data: new SlashCommandBuilder()
     .setName("reset")
     .setDescription("Replies with next raid reset."),
   async execute(interaction) {
-    const rightNow = Date.now();
-    let nextReset = Date.parse("21 Dec 2023 10:00:00 EST");
-
-    // Once rightNow is equal or lower than nextReset, nextReset gains 72 hours
-    if (rightNow >= nextReset) {
-      nextReset = nextReset + 72 * 3600000;
-    }
-
     const nextDate = new Date(nextReset).toLocaleDateString();
 
     let timeLeft = nextReset - rightNow;
